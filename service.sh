0eb3b3d24fafca02ccf203d8c080a6cd9a2aa008
LiZeC <tomgreem123@gmail.com>
2021-11-02 09:38:49 +0800

    添加更新指令的判断

--- service.sh
+++ service.sh
@@ -14,11 +14,14 @@ function update() {
 
 if [ "$1"x == "backup"x ]; then
   backup
+elif [ "$1"x == "update"x ]; then
+  update
 else
   echo "无效的参数: $1"
   echo ""
   echo "用法: ./service [参数]"
   echo "参数可以选择以下值:"
   echo "backup    备份数据文件"
+  echo "update    更新项目代码并重启"
   echo ""
 fi
